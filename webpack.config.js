75803acbb0294944415567e16a2862d0f980c3e6
Daniyil Vasylenko <danielspr3@gmail.com>
2018-03-13 19:28:46 +0200

    Set up

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReloadPlugin = require('reload-html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const PurgecssPlugin = require('purgecss-webpack-plugin');
+const purgeHtml = require('purgecss-from-html');
+const nodeExternals = require('webpack-node-externals');
+const path = require('path');
+const glob = require('glob-all');
+const tailwindcss = require('tailwindcss');
+
+const isRelease = process.argv.indexOf('-p') !== -1;
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'styles.css'
+  },
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: ['node_modules/'],
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1,
+                minimize: isRelease
+              }
+            },
+            {
+              loader: 'postcss-loader'
+            }
+          ]
+        })
+      }
+    ]
+  },
+  devServer: {
+    contentBase: __dirname,
+    watchContentBase: true
+  },
+  plugins: [
+    new ExtractTextPlugin("styles.css"), 
+    new ReloadPlugin(),
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    }),
+    new PurgecssPlugin({
+      paths: glob.sync([
+        path.join(__dirname, 'index.html')
+      ]),
+      extractors: [{
+        extractor: purgeHtml,
+        extensions: ['html']
+      }]
+    })
+  ]
+}
